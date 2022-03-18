ddfcbc96e4f58032698753760a837cf7334cab1e
Kamil Kowalczyk <kkowalczyk@fis-sst.pl>
2022-03-18 02:50:38 +0100

    Nowasza wersja eslinta oraz lepsza konfiguracja i poprawki aby kod był z nią zgodny

--- frontend/editor/src/App.test.tsx
+++ frontend/editor/src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('test if app is working and have title', () => {
+test("if app is working and have title", () => {
   render(<App />);
   expect(screen.getByText(/Login/i)).toBeInTheDocument();
 });
