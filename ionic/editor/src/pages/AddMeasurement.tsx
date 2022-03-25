12a30398684bbeb76eab4dacd0da38a6d51b1a04
Kamil Kowalczyk <kkowalczyk@fis-sst.pl>
2022-03-25 13:32:15 +0100

    Dodanie natywnej obsługi geolokalizacji oraz powiadomień podczas dodawania pomieru w aplikacji mobilnej

--- ionic/editor/src/pages/AddMeasurement.tsx
+++ ionic/editor/src/pages/AddMeasurement.tsx
@@ -1,32 +1,34 @@
 import { IonButton, IonContent, IonInput, IonPage, IonSelect, IonSelectOption } from '@ionic/react';
 import { useState } from 'react';
 import { CreateMeasurement, DataService } from '../api';
-import Confirm from '../components/Confirm';
 import { tags } from '../interfaces/tags';
+import { Geolocation } from '@capacitor/geolocation';
+import Confirm from '../components/Confirm';
+import showInfo from '../components/Notification';
 import "./AddMeasurement.css";
 
 export default function AddMeasurement() {
     const [previousId, setPreviousId] = useState<number>(0);
 
-    function handleSubmit(e: any) {
+    async function handleSubmit(e: any) {
         e.preventDefault();
-        if (!e.target.elements.title.value) {
-            // enqueueSnackbar('Please fill title', { variant: 'error' });
+        if (!e.target.elements.laeq.value) {
+            showInfo('Please fill laeq', 'error');
             return;
         }
-
-        navigator.geolocation.getCurrentPosition((position) => {
-            let latitude = position.coords.latitude;
-            let longitude = position.coords.longitude;
-            if (latitude && longitude && window) {
+            Geolocation.getCurrentPosition().then(res => {    
+                let latitude = res.coords.latitude.valueOf();
+                let longitude = res.coords.longitude.valueOf();
+                if (latitude != null && longitude != null) {
                 let date = new Date();
-                let tags = e.target.elements.tags.value.split(',');
+                let tagArray = e.target.elements.tags.value.split(',');
                 date.setHours(date.getHours() + 1);
                 const measurementBody: CreateMeasurement = {
                     title: e.target.elements.title.value,
                     description: e.target.elements.description.value,
                     notes: e.target.elements.notes.value,
-                    tags: tags,
+                    tags: tagArray,
+                    laeq: e.target.elements.laeq.value,
                     location: {
                         latitude,
                         longitude,
@@ -34,27 +36,22 @@ export default function AddMeasurement() {
                     }
                 };
                 DataService.addMeasurementApiDataCreatePost(measurementBody).then(res => {
-                    // enqueueSnackbar('The measurement was added', {
-                    //     variant: 'success',
-                    // });
-                    // window.open(`https://www.google.com/search?q=${latitude} ${longitude}`, '_blank'); // for google search
+                    showInfo('The measurement was added', 'success');
                     window.open(`https://www.google.com/maps/place/${latitude} ${longitude}`, '_blank'); // for google maps
                     setPreviousId(res.measurement_id);
                 }).catch(err => {
-                    // enqueueSnackbar('Ops! We have some error check your internet connection or login again', {
-                    //     variant: 'error',
-                    // });
-                    console.log(err);
+                    showInfo('Ops! We have some error check your internet connection or login again', 'error');
                 });
             }
+        }).catch(err => {
+            Geolocation.requestPermissions();
+            showInfo('Ops! problem with location', 'error');
         });
     }
 
     function report() {
-        
-        window.location.href = `/editor/edit/${previousId}`; // with refresh
-        // window.history.pushState({}, '', `/editor/edit/${previousId}`);
-        // window.dispatchEvent(new PopStateEvent('popstate')); // without refresh
+        window.history.pushState({}, '', `/editor/edit/${previousId}`);
+        window.dispatchEvent(new PopStateEvent('popstate')); // without refresh
     }
 
     return (
