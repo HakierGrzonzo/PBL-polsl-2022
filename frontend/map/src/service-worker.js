49a09dfe47687622854c2204027cb88240bc1c27
Kamil Kowalczyk <kkowalczyk@fis-sst.pl>
2022-04-02 08:17:23 +0200

    Eksperymenty z pwa

--- frontend/map/src/service-worker.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-
-
-workbox.routing.registerRoute(
-  ({request}) => request.destination === 'script',
-  new workbox.strategies.NetworkFirst()
-);
